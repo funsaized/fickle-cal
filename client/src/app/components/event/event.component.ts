2a2041994b636feeb3cdb1a8a2d078f1af5f4d6b
snimmagadda1 <saiguy@me.com>
2024-09-12 20:25:53 -0400

    CSS almost doesnt suck for input on hover & click

--- client/src/app/components/event/event.component.ts
+++ client/src/app/components/event/event.component.ts
@@ -9,25 +9,34 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   template: `
     <div
       class="input-root"
+      (mouseover)="onMouseOver()"
+      (mouseout)="onMouseOut()"
       (click)="onFocus()"
       (focusin)="onFocus()"
-      (focusout)="onBlur()"
-      [ngClass]="focused ? 'focus' : ''"
+      (focusout)="focused = false"
+      [ngClass]="{ hover: hovered, focus: focused }"
     >
-      <input id="name" type="text" [formControl]="text" />
+      <input id="name" type="text" [formControl]="text" autocomplete="off"/>
     </div>
   `,
   styleUrl: './event.component.scss',
 })
 export class EventComponent {
   focused = false;
+  hovered = false;
   text = new FormControl('');
 
   onFocus() {
     this.focused = true;
   }
 
-  onBlur() {
-    this.focused = false;
+  onMouseOver() {
+    console.log('RUNNING OVER');
+    this.hovered = true;
+  }
+
+  onMouseOut() {
+    console.log('RUNNING OUT');
+    this.hovered = false;
   }
 }
