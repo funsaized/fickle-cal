9fd568dd55e541a928b032d87ab7be3b2b669e00
snimmagadda1 <saiguy@me.com>
2025-02-06 06:06:18 -0500

    checkpoint.. im out tho

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,15 @@
+process.env.NODE_ENV = "test";
+process.env.GITHUB_CLIENT_ID = "testClientId";
+process.env.GITHUB_CLIENT_SECRET = "testClientSecret";
+process.env.GITHUB_CALLBACK_URL = "http://localhost:8080/auth/github/callback";
+process.env.SESSION_SECRET = "test-secret-key";
+process.env.FRONTEND_URL = "http://localhost:4200";
+
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  transformIgnorePatterns: ["node_modules"],
 };
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["development", "production"]),
+  NODE_ENV: z.enum(["development", "production", "test"]),
   SESSION_SECRET: z.string(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
