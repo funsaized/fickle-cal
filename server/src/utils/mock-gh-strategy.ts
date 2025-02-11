4498207c9f7041a13f2be13fcfcbaeddbc2dfd19
snimmagadda1 <saiguy@me.com>
2025-02-11 06:26:17 -0500

    Minor refactor of mock strategy

--- server/src/utils/mock-gh-strategy.ts
+++ server/src/utils/mock-gh-strategy.ts
@@ -1,16 +1,21 @@
 import passport from "passport";
+import { type Profile as GitHubProfile } from "passport-github2";
 
 // The reply from Github OAuth2
 import user from "./gh-mock-profile";
 
 export default class MockGHStrategy extends passport.Strategy {
-  _cb: any;
-  _user: any;
+  _cb: (
+    accessToken: string,
+    refreshToken: string,
+    profile: GitHubProfile,
+    done: (a: unknown, user: Express.User | false) => void
+  ) => void;
+  _user: GitHubProfile;
 
   constructor(name: string, cb: any) {
     super();
 
-    // Set the default name of our strategy
     this.name = name;
     this._cb = cb;
     this._user = user;
@@ -19,16 +24,16 @@ export default class MockGHStrategy extends passport.Strategy {
   /**
    * Authenticate a request.
    *
-   * This function should call exactly one of `this.success(user, info)`, `this.fail(challenge, status)`,
+   * `this.success(user, info)`, `this.fail(challenge, status)`,
    * `this.redirect(url, status)`, `this.pass()`, or `this.error(err)`.
-   * See https://github.com/jaredhanson/passport-strategy#augmented-methods.
+   * https://github.com/jaredhanson/passport-strategy#augmented-methods.
    *
    * @param {Object} req - Request.
    * @param {Object} options - The options object passed to `passport.authenticate()`.
    * @return {void}
    */
   authenticate(req: any, options: any) {
-    this._cb(null, null, this._user, (err: any, user: any) => {
+    this._cb("N/A", "N/A", this._user, (err: any, user: any) => {
       this.success(user);
     });
   }
